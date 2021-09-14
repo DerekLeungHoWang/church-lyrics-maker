06487e7e4ac129db015f6b7534d155bccd2185a3
DerekLeungHoWang <hkz88i00123@gmail.com>
2021-09-14 21:25:30 +0800

    update [skip ci]

--- src/modules/components/LyricsMaker/MakerForm/Image/ImageUploader.js
+++ src/modules/components/LyricsMaker/MakerForm/Image/ImageUploader.js
@@ -1,29 +1,63 @@
-import { Grid,Button, Container, Modal, Paper } from '@material-ui/core'
+import { Grid, Button, Container, Modal, Paper, makeStyles, Typography, Divider } from '@material-ui/core'
 import React, { useState } from 'react'
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+}
 
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+
+
+    }
+}));
+function getModalStyle() {
+    const top = 50
+    const left = 50
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`
+    };
+}
 export default function ImageUploader() {
+    const classes = useStyles()
     const [open, setOpen] = useState(false)
-
+    const [modalStyle] = useState(getModalStyle);
 
     const handleClose = () => {
         setOpen(false)
     }
     return (
-        <Container>
+        <Grid container justifyContent="center" alignItems="center">
             <Button onClick={() => setOpen(!open)}>Upload an image</Button>
             <Modal
+                style={{
+                    display: 'flex', alignItems: 'center', justifyContent: 'center',
+
+                }}
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="simple-modal-title"
                 aria-describedby="simple-modal-description"
             >
-                <Container maxWidth={false} >
-                    <Paper style={{ maxWidth: "80%", maxHeight: "80%", top:"50%",left:"50%" }}>
-                        Hello
-                    </Paper>
-                </Container>
-            </Modal>
-        </Container>
 
+
+                <Paper style={{
+                    height: "90%",
+                    minWidth: "80%",
+                    padding: "15px",
+
+                }} >
+                    <Typography variant="h6" >Image Upload</Typography>
+                    <Divider style={{width:"100%"}}/>
+                </Paper>
+
+            </Modal>
+        </Grid>
     )
 }
